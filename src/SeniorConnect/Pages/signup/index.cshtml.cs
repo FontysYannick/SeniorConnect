a351b741fd8d8164fd3b3565e1003b455d2facf9
misstodis <nduc2312000@hotmail.com>
2024-05-14 18:23:00 +0200

    signup page

---
+++ src/SeniorConnect/Pages/signup/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeniorConnect.Pages.signup
+{
+    public class signupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
