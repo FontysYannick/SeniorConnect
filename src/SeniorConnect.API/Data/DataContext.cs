44a529ea0500600cf0fcf8bf5134b7eb083ce902
misstodis <nduc2312000@hotmail.com>
2024-05-23 11:45:48 +0200

    initial Activities table

--- src/SeniorConnect.API/Data/DataContext.cs
+++ src/SeniorConnect.API/Data/DataContext.cs
@@ -6,10 +6,11 @@ namespace SeniorConnect.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options): base(options)
         {
-            
         }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -19,6 +20,13 @@ namespace SeniorConnect.API.Data
             modelBuilder.Entity<User>()
                 .HasIndex(u => u.Email)
                 .IsUnique();
+
+
+            // Define relationship between User and Activity
+            modelBuilder.Entity<Activity>()
+                .HasOne(a => a.Organizer)
+                .WithMany(u => u.Activities)
+                .HasForeignKey(a => a.OrganizerId);
         }
     }
 }
--- src/SeniorConnect.API/Entities/Activity.cs
+++ src/SeniorConnect.API/Entities/Activity.cs
@@ -2,16 +2,21 @@
 {
     public class Activity
     {
-        public long ActivityId { get; set; }
+        public int ActivityId { get; set; }
         public string Title { get; set; }
-        public BaseListItem Catagory { get; set; }
+        public int OrganizerId { get; set; }
         public User Organizer { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTime Date { get; set; }
         public string Place { get; set; }
         public int? MaxParticipants { get; set; }
-        public decimal? Price { get; set; }
+        public string? Awards { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
-        public Activity() { }
+        public Activity()
+        {
+            
+        }
     }
 }
